1c791750ef8db39bc04e63fa5790d0b8ff1544c4
akrigline <andrew@akrigline.com>
2015-03-11 22:34:52 -0400

    added overlay, removed flipthroughs

--- js/main.js
+++ js/main.js
@@ -69,7 +69,8 @@ if (document.getElementById('home')) {
         //Initialize Parllax
         $('#scene').parallax({
             relativeInput: true,
-            clipRelativeInput: true
+            clipRelativeInput: true,
+            limitY:0
         });
 
         //Initialize Sticky.js
@@ -106,6 +107,19 @@ if (document.getElementById('home')) {
                 speed: 7500
             });
         }, 3750 );
+
+        setTimeout( function (){
+            $(".scrollThroughOffset2 h1").css('display', 'none');
+            $(".scrollThroughOffset2 .hideme").css('display', 'inline-block');
+            $(".scrollThroughOffset2 .hideme").Morphext({
+                // The [in] animation type. Refer to Animate.css for a list of available animations.
+                animation: "flipInX",
+                // An array of phrases to rotate are created based on this separator. Change it if you wish to separate the phrases differently (e.g. So Simple | Very Doge | Much Wow | Such Cool).
+                separator: ",",
+                // The delay between the changing of each phrase in milliseconds.
+                speed: 7500
+            });
+        }, 2250 );
 
     });
 
